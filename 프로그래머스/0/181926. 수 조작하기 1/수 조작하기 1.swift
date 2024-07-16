3edf4ca316ab7ee36db30ae842395c5e5732be91
Suyeon <81567790+syhwang1231@users.noreply.github.com>
2024-07-17 02:12:28 +0900

    [level 0] Title: 수 조작하기 1, Time: 11.51 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.swift
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.swift
@@ -1,20 +1,7 @@
 import Foundation
 
 func solution(_ n:Int, _ control:String) -> Int {
-    var n = n
-    control.forEach {
-        switch ($0) {
-        case "w":
-            n += 1
-        case "s":
-            n -= 1
-        case "d":
-            n += 10
-        case "a":
-            n -= 10
-        default:
-            return
-        }
-    }
-    return n
+    let controls: [Character:Int] = ["w":1, "s":-1, "d":10, "a":-10]
+    
+    return n + control.reduce(0) { $0 + controls[$1]! }
 }
