12fb0643f9ce9d4e296ced34cfd28569bb039e95
Suyeon <81567790+syhwang1231@users.noreply.github.com>
2024-07-16 16:49:40 +0900

    [level 0] Title: 코드 처리하기, Time: 25.09 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.swift
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.swift
@@ -1,27 +1,20 @@
 import Foundation
 
 func solution(_ code:String) -> String {
-    var mode = 0
+    var mode = false
     var ret = ""
     
     code.enumerated().forEach { (index, element) in
-        if mode == 0 {
-            if element == "1" {
-                mode = 1
-            }
-            else if index % 2 == 0 {
-                ret += String(element)
-            }
+        let code = String(element)
+                               
+        if element == "1" {
+            mode.toggle()
         }
-        else {
-            if element == "1" {
-                mode = 0
-            }
-            else if index % 2 == 1 {
-                ret += String(element)
-            }
+        
+        else if (!mode && index % 2 == 0) || (mode && index % 2 == 1) {
+            ret += code
         }
     }
     
-    return ret == "" ? "EMPTY" : ret
+    return ret.isEmpty ? "EMPTY" : ret
 }
