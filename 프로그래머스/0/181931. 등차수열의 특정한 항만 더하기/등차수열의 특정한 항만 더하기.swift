ed889655bce3b16fc311fb18bf0981841d541dab
Suyeon <81567790+syhwang1231@users.noreply.github.com>
2024-07-16 17:43:07 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.20 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.swift
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.swift
@@ -2,10 +2,9 @@ import Foundation
 
 func solution(_ a:Int, _ d:Int, _ included:[Bool]) -> Int {
     var result:Int = 0
-    for i in 0 ..< included.count {
-        if included[i] {
-            result += a + d * i
-        }
+    included.indices.filter { included[$0] }.map {
+        result += a + d * $0
     }
+    
     return result
 }
