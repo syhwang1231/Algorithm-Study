b01f405af451b503e19226f6970a9abcb653c69c
Suyeon <81567790+syhwang1231@users.noreply.github.com>
2024-07-19 18:54:19 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 126.49 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.swift
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.swift
@@ -3,10 +3,10 @@ import Foundation
 func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
     return queries.reduce(into: arr) { result, q in
         let (s, e, k) = (q[0], q[1], q[2])
-        (s...e).filter {
-            $0.isMultiple(of: k)
-        }.map {
-            result[$0] += 1
+        (s...e).forEach {
+            if $0.isMultiple(of: k) {
+                result[$0] += 1
+            }
         }
     }
 }
