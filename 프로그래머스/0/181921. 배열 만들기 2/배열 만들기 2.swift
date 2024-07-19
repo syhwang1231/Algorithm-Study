18551ec82d0fc4a20cc9cec14f55f13f86684383
Suyeon <81567790+syhwang1231@users.noreply.github.com>
2024-07-19 19:53:52 +0900

    [level 0] Title: 배열 만들기 2, Time: 2311.27 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.swift
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.swift
@@ -4,7 +4,7 @@ func solution(_ l:Int, _ r:Int) -> [Int] {
     var arr: [Int] = (l...r).reduce(into: []) { result, num in
         let numStr = String(num)
         let numSet = Set(numStr)
-        if numSet == ["0", "5"] || numSet == ["0"] || numSet == ["5"] {
+        if numSet.isSubset(of: ["0", "5"]) {
             result.append(Int(numStr)!)
         }
     }
