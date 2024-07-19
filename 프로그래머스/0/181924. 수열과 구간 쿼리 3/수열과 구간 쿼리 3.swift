caab8503314d857999d0a6d3da6fd66b01c45165
Suyeon <81567790+syhwang1231@users.noreply.github.com>
2024-07-19 17:15:16 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.54 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.swift
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.swift
@@ -1,12 +1,7 @@
 import Foundation
 
 func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
-    var numArr = arr
-    var tmp: Int = 0
-    queries.map { 
-        tmp = numArr[$0[0]]
-        numArr[$0[0]] = numArr[$0[1]]
-        numArr[$0[1]] = tmp
+    return queries.reduce(into: arr) { result, query in
+        result.swapAt(query[0], query[1])
     }
-    return numArr
 }
