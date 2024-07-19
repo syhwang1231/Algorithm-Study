3e0615741868cf14d30d8c7c0d499faa4ef1a91a
Suyeon <81567790+syhwang1231@users.noreply.github.com>
2024-07-19 16:48:14 +0900

    [level 0] Title: 수 조작하기 2, Time: 35.57 ms, Memory: 22.4 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.swift
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.swift
@@ -2,9 +2,5 @@ import Foundation
 
 func solution(_ numLog:[Int]) -> String {
     let controls: [Int:String] = [1:"w", -1:"s", 10:"d", -10:"a"]
-    var result: String = ""
-    for i in 1 ..< numLog.count {
-        result += controls[numLog[i] - numLog[i-1]]!
-    }
-    return result
+    return (1 ..< numLog.count).map{ controls[numLog[$0] - numLog[$0-1]]! }.joined()
 }
