4cffbeb7f67b6b0a34025c7de623034fb00bc843
Suyeon <81567790+syhwang1231@users.noreply.github.com>
2024-07-22 01:12:47 +0900

    [level 0] Title: 배열 만들기 4, Time: 41.58 ms, Memory: 20.9 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.swift
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.swift
@@ -5,18 +5,12 @@ func solution(_ arr:[Int]) -> [Int] {
     var stk: [Int] = []
     
     while(i < arr.count) {
-        if stk.isEmpty {
+        if stk.isEmpty || stk.last! < arr[i] {
             stk.append(arr[i])
             i += 1
         }
         else {
-            if stk.last! < arr[i] {
-                stk.append(arr[i])
-                i += 1
-            }
-            else {
-                stk.removeLast()
-            }
+            stk.removeLast()
         }
     }
     
