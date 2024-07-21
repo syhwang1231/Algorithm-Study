320c4e19cddc100d5630ece5bd69e2df2c581122
Suyeon <81567790+syhwang1231@users.noreply.github.com>
2024-07-22 01:03:02 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.swift
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.swift
@@ -1,17 +1,13 @@
 import Foundation
 
 func solution(_ n:Int) -> [Int] {
-    return collatz(n: n, arr: [])
-}
-
-func collatz(n: Int, arr: [Int]) -> [Int] {
-    var arr = arr
-    arr.append(n)
+    var arr: [Int] = []
+    var num: Int = n
     
-    if n == 1 {
-        return arr
+    while(num != 1) {
+        arr.append(num)
+        num = (num % 2 == 0) ? num / 2 : 3 * num + 1
     }
-    
-    let result: Int = n % 2 == 0 ? n / 2 : 3 * n + 1
-    return collatz(n: result, arr: arr)
+    arr.append(1)
+    return arr
 }
