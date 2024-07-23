6a218a79047c67be44f2114de70602b117c6efe7
Suyeon <81567790+syhwang1231@users.noreply.github.com>
2024-07-23 18:55:15 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 134.41 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.swift
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.swift
@@ -2,13 +2,10 @@ import Foundation
 
 func solution(_ my_string:String, _ queries:[[Int]]) -> String {
     var arr = Array(my_string)
-    return String(queries.reduce(into: arr) { arr, query in
-        var s = query[0]
-        var e = query[1]
-        while (s <= e) {
-            arr.swapAt(s, e)
-            s += 1
-            e -= 1
-        }
-    })
+    
+    queries.forEach { q in
+        arr.replaceSubrange(q[0]...q[1], with: arr[q[0]...q[1]].reversed())
+    }
+    
+    return String(arr)
 }
