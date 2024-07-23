bc28465acbea8716ea529388fa48206a40ea4803
Suyeon <81567790+syhwang1231@users.noreply.github.com>
2024-07-23 19:28:14 +0900

    [level 0] Title: 배열 만들기 5, Time: 65.11 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.swift
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.swift
@@ -1,12 +1,8 @@
 import Foundation
 
 func solution(_ intStrs:[String], _ k:Int, _ s:Int, _ l:Int) -> [Int] {
-    var arr: [Int] = []
-    intStrs.forEach {
+    return intStrs.map {
         let num = Int(String(Array($0)[s...(s + l - 1)]))!
-        if num > k {
-            arr.append(num)
-        }
-    }
-    return arr
+        return num
+    }.filter { $0 > k }
 }
