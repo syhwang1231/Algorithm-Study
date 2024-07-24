2e372f8a5d160a6fa99ba4a47ceb7688a8450e46
Suyeon <81567790+syhwang1231@users.noreply.github.com>
2024-07-24 19:57:30 +0900

    [level 0] Title: 접미사 배열, Time: 0.59 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.swift
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.swift
@@ -1,9 +1,7 @@
 import Foundation
 
 func solution(_ myString:String) -> [String] {
-    var arr: [String] = []
-    for i in 1 ... myString.count {
-        arr.append(String(myString.suffix(i)))
-    }
-    return arr.sorted()
+    return myString.indices.map {
+        String(myString[$0...])
+    }.sorted()
 }
